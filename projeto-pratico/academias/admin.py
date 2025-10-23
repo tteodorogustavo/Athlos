ba9dfb4b74cfd6ec64ea4373b7282c830c76a0f9
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2025-10-23 09:45:29 -0300

    feat: Atualizar admin para modelos de Academia, PersonalTrainer e Aluno; adicionar filtros e ordenação

--- projeto-pratico/academias/admin.py
+++ projeto-pratico/academias/admin.py
@@ -1,10 +1,41 @@
 from django.contrib import admin
 from .models import Academia, PersonalTrainer, Aluno
 
+
 @admin.register(Academia)
 class AcademiaAdmin(admin.ModelAdmin):
-    list_display = ('nome_fantasia', 'cnpj', 'endereco', 'telefone')
-    search_fields = ('nome_fantasia', 'cnpj')
+    list_display = ("nome_fantasia", "cnpj", "endereco", "telefone", "data_criacao")
+    search_fields = ("nome_fantasia", "cnpj", "endereco")
+    list_filter = ("data_criacao",)
+    ordering = ("-data_criacao",)
 
-admin.site.register(PersonalTrainer)
-admin.site.register(Aluno)
+
+@admin.register(PersonalTrainer)
+class PersonalTrainerAdmin(admin.ModelAdmin):
+    list_display = ("user", "cref", "especialidade", "get_academia")
+    search_fields = ("user__first_name", "user__last_name", "cref", "user__email")
+    list_filter = ("especialidade", "user__academia", "user__date_joined")
+    ordering = ("user__first_name",)
+
+    def get_academia(self, obj):
+        return obj.user.academia
+
+    get_academia.short_description = "Academia"
+
+
+@admin.register(Aluno)
+class AlunoAdmin(admin.ModelAdmin):
+    list_display = ("user", "personal_responsavel", "data_nascimento", "get_academia")
+    search_fields = ("user__first_name", "user__last_name", "user__email", "objetivo")
+    list_filter = (
+        "personal_responsavel",
+        "user__academia",
+        "data_nascimento",
+        "user__date_joined",
+    )
+    ordering = ("-user__date_joined",)
+
+    def get_academia(self, obj):
+        return obj.user.academia
+
+    get_academia.short_description = "Academia"
--- projeto-pratico/treinos/admin.py
+++ projeto-pratico/treinos/admin.py
@@ -4,21 +4,37 @@ from .models import Exercicio, Treino, ItemTreino
 
 class ItemTreinoInline(admin.TabularInline):
     model = ItemTreino
-    extra = 1 
+    extra = 1
+
 
 @admin.register(Exercicio)
 class ExercicioAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'level', 'equipment', 'category')
-    search_fields = ('nome', 'category', 'equipment') 
-    list_filter = ('level', 'equipment', 'category')
+    list_display = ("nome", "level", "equipment", "category")
+    search_fields = ("nome", "category", "equipment")
+    list_filter = ("level", "equipment", "category")
+
 
 @admin.register(Treino)
 class TreinoAdmin(admin.ModelAdmin):
     """
     Customiza a tela de admin para a Ficha de Treino.
     """
-    list_display = ('nome_treino', 'aluno', 'personal_criador', 'ativo')
-    list_filter = ('ativo', 'personal_criador')
+
+    list_display = ("nome_treino", "aluno", "personal_criador", "data_criacao", "ativo")
+    list_filter = (
+        "ativo",
+        "personal_criador",
+        "data_criacao",
+        "aluno__user__academia",
+    )
+    search_fields = (
+        "nome_treino",
+        "aluno__user__first_name",
+        "aluno__user__last_name",
+        "personal_criador__user__first_name",
+        "personal_criador__user__last_name",
+    )
+    ordering = ("-data_criacao",)
 
     # possibilita editar os Itens de Treino diretamente na página do Treino
     inlines = [ItemTreinoInline]
